4fe601c38490f84296d8e52f94c408ccb59ba78a
Evgenii Mikhailov <e.mikhailov@outlook.com>
2024-02-09 15:14:27 +0300

    Initial Commit

---
+++ BroadApps_SwiftUI/BroadApps_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BroadApps_SwiftUIApp.swift
+//  BroadApps_SwiftUI
+//
+//  Created by Evgenii Mikhailov on 09.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct BroadApps_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BroadApps_SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BroadApps_SwiftUI
+//
+//  Created by Evgenii Mikhailov on 09.02.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
